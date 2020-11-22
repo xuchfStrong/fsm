e53d5ea6c877d98d05d4d16d2dd81ff6ff11b941
xuchangfan <xuchangfan@jieyundata.com>
2020-11-22 15:15:07 +0800

    第一次提交

---
+++ pages/update/update.vue
@@ -0,0 +1,176 @@
+<template>
+  <view class="content">
+    <view class="summary-wrap">
+        <!-- <view v-if="hasNewVersion" class="highlight">有新版本，请下载新的版本安装，当前版本为V{{ $global.fuzhuVersionName }}</view> -->
+        <view v-if="showUpdate">
+          <button type="primary" plain="true" size="mini" @tap="handleUpdate">更新</button>
+				  <text style="width: 10upx; display: inline-block;"></text>
+				  <button type="primary" plain="true" size="mini" @tap="hideButton">收起</button>
+        </view>
+        <view>当前版本为V{{ $global.fuzhuVersionName }}</view>
+				<view>更新说明：</view>
+        <view class="summary-content">
+          <view v-for="(item, index) in utils.updateDescription" :key="index">{{ item }}</view>
+        </view>
+
+       <view style="margin-top:10px; color:#1989fa;">
+          <a v-if="saleChannel==='1'" :href="utils.apkDownloadUrl1">
+            <text>点击下载辅助APP</text>
+          </a>
+          <a v-if="saleChannel==='2'" :href="utils.apkDownloadUrl2">
+            <text>点击下载辅助APP</text>
+          </a>
+          <a v-if="saleChannel==='3'" :href="utils.apkDownloadUrl3">
+            <text>点击下载辅助APP</text>
+          </a>
+          <a v-if="saleChannel==='4'" :href="utils.apkDownloadUrl4">
+            <text>点击下载辅助APP</text>
+          </a>
+					<a v-if="saleChannel==='5'" :href="utils.apkDownloadUrl5">
+					  <text>点击下载辅助APP</text>
+					</a>
+          <a v-if="saleChannel==='6'" :href="utils.apkDownloadUrl6">
+            <text>点击下载辅助APP</text>
+          </a>
+          <a v-if="saleChannel==='7'" :href="utils.apkDownloadUrl7">
+            <text>点击下载辅助APP</text>
+          </a>
+          <a v-if="saleChannel==='8'" :href="utils.apkDownloadUrl8">
+            <text>点击下载辅助APP</text>
+          </a>
+          <!-- <button type="primary" @tap="downloadImage">下载</button> -->
+        </view>
+      </view>
+  </view>
+</template>
+
+<script>
+import { getUtils, getUpdate } from '@/api/game'
+import { getChannel } from '@/utils/index'
+export default {
+  data() {
+    return {
+      saleChannel: '',
+      utils: '',
+      showUpdate: false
+    }
+  },
+  onLoad() {
+    this.saleChannel = getChannel()
+    this.handleGetUtils()
+  },
+
+  onPullDownRefresh() {
+      console.log('refresh');
+      this.showUpdate = true
+      setTimeout(function () {
+          uni.stopPullDownRefresh();
+      }, 1000);
+  },
+
+  computed: {
+    hasNewVersion() {
+      const newVersion = this.utils.version
+      const currentVersion = this.$global.fuzhuVersion
+      return newVersion > currentVersion
+    }
+  },
+
+  methods: {
+    handleGetUtils() {
+      getUtils().then(res => {
+        this.utils = res
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+
+    downloadImage: function () {
+			uni.showLoading({
+				title:'下载中'
+			})
+			var self = this
+			uni.downloadFile({
+				url: this.utils.apkDownloadUrl,
+				success: (res) => {
+					console.log('downloadFile success, res is', res)
+					self.imageSrc = res.tempFilePath;
+					uni.hideLoading();
+				},
+				fail: (err) => {
+					console.log('downloadFile fail, err is:', err)
+				}
+			})
+    },
+
+    hideButton() {
+      this.showUpdate = false
+    },
+    
+    handleUpdate() {
+      // #ifdef APP-PLUS  
+			const that = this
+			plus.runtime.getProperty(plus.runtime.appid, function() {
+        	getUpdate().then(result => {
+            var data = result;
+            var wgtUrl = data.test.wgtUrl
+            var pkgUrl = data.test.pkgUrl
+			      if (data.test.version > that.$global.fuzhuVersion && wgtUrl && data.test.updateType === 1) { // 热更新
+			          uni.downloadFile({  
+			              url: wgtUrl,  
+			              success: (downloadResult) => {  
+			                  if (downloadResult.statusCode === 200) {  
+			                      plus.runtime.install(downloadResult.tempFilePath, {  
+			                          force: true  
+			                      }, function() {  
+			                          console.log('install success...');  
+																uni.showToast({
+																	title:"辅助更新成功，即将重启",
+																	duration:1000,
+																	icon:'none'
+																})
+																setTimeout(function() {
+																  plus.runtime.restart();
+																}, 1100)
+			                      }, function(e) {  
+			                          console.error('install fail...'); 
+																 uni.showToast({
+																 	title:"辅助更新失败",
+																	duration:2000,
+																	icon:'none'
+																 })
+			                      });  
+			                  }
+			              }  
+			          }); 
+            }
+            if (data.test.version > that.$global.fuzhuVersion && pkgUrl && data.tesst.updateType === 2 ) { //整包更新
+							uni.showModal({ //提醒用户更新  
+								title: "更新提示",  
+								content: data.test.note,  
+								success: (res) => {  
+									if (res.confirm) {  
+										plus.runtime.openURL(pkgUrl);  
+									}  
+								}  
+							})  
+						}  
+          })
+			});  
+			// #endif
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .summary-wrap {
+    padding: 0 15upx;
+  }
+  .summary-content {
+    color: #969799;
+  }
+  .highlight {
+    color: red;
+  }
+</style>
